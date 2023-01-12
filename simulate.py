a1fe4f9928a86789acf22317aa60b85840ab4fc3
Charles Bugayer <chaimbugayer2024@u.northwestern.edu>
2023-01-12 12:52:24 -0600

    next day

--- simulate.py
+++ simulate.py
@@ -1,8 +1,14 @@
+import pybullet_data
 import pybullet as p
 import time as time
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+p.setGravity(0,0,-9.8)
+planeId = p.loadURDF("plane.urdf")
+RobotId = p.loadURDF("body.urdf")
+p.loadSDF("world.sdf")
 for i in range(1000):
     print(i)
-    time.sleep(i)
+    time.sleep(1/60)
     p.stepSimulation()
 p.disconnect()
