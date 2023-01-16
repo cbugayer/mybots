91853e5907c777e06c8f9ade9f9bbc8bdf9eac97
Charles Bugayer <chaimbugayer2024@u.northwestern.edu>
2023-01-15 18:22:29 -0600

    midmotor

--- analyze.py
+++ analyze.py
@@ -2,8 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 backLegSensorValues = np.load("data/sensordata.npy")
 frontLegSensorValues = np.load("data/sensordatafront.npy")
+sinusodal = np.load("data/sinus.npy")
+plt.plot(sinusodal)
 #print(backLegSensorValues)
-plt.plot(backLegSensorValues, label = "Back", linewidth = 3)
-plt.plot(frontLegSensorValues, label= "Front")
+#plt.plot(backLegSensorValues, label = "Back", linewidth = 3)
+#plt.plot(frontLegSensorValues, label= "Front")
 plt.legend()
 plt.show()
--- simulate.py
+++ simulate.py
@@ -2,6 +2,7 @@ import pybullet_data
 import pybullet as p
 import numpy as np
 import pyrosim.pyrosim as pyrosim
+import random
 
 import time as time
 physicsClient = p.connect(p.GUI)
@@ -13,6 +14,8 @@ p.loadSDF("world.sdf")
 pyrosim.Prepare_To_Simulate(RobotId)
 backLegSensorValues = np.zeros(1000)
 frontLegSensorValues = np.zeros(1000)
+targetAngles = np.sin(np.linspace(0,  2*np.pi, 1000))*(np.pi/4)
+#np.save(file = "data/sinus.npy", arr = targetAngles)
 for i in range(1000):
     #print(i)
     time.sleep(1/60)
@@ -20,6 +23,19 @@ for i in range(1000):
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("Backleg")    
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("Frontleg")
     #print(backLegSensorValues)
+    
+    pyrosim.Set_Motor_For_Joint(
+        bodyIndex = RobotId,
+        jointName = b'Torso_Backleg', 
+        controlMode = p.POSITION_CONTROL,
+        targetPosition = (-np.pi/2.0)+(2*(np.pi/2.0)*random.random()),
+        maxForce = 40)
+    pyrosim.Set_Motor_For_Joint(
+        bodyIndex = RobotId,
+        jointName = b'Torso_Frontleg', 
+        controlMode = p.POSITION_CONTROL,
+        targetPosition = (-np.pi/2.0)+(2*(np.pi/2.0)*random.random()),
+        maxForce = 40)
     np.save(file = "data/sensordata.npy", arr = backLegSensorValues)
     np.save(file = "data/sensordatafront.npy", arr = frontLegSensorValues)
 p.disconnect()
