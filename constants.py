793969d066636cf2a6f0ddd489611995dd3fa630
Charles Bugayer <chaimbugayer2024@u.northwestern.edu>
2023-01-15 20:58:56 -0600

    refractoring26

---
+++ constants.py
@@ -0,0 +1,20 @@
+import numpy as np
+steps = 1000
+
+gravity = -9.8
+BLamplitude = np.pi/2
+BLfrequency = 10
+BLphaseOffset = np.pi/4
+
+BLmaxForce = 40
+
+FLamplitude = np.pi/2
+FLfrequency = 1000
+FLphaseOffset = np.pi
+
+FLmaxForce = 40
+
+target_angles = 2*np.pi
+
+time_sleep = 1/60
+
--- simulate.py
+++ simulate.py
@@ -4,53 +4,48 @@ import numpy as np
 import pyrosim.pyrosim as pyrosim
 import random
 import time as time
+import constants as c
+from simulation import SIMULATION 
 
-BLamplitude = np.pi/2
-BLfrequency = 10
-BLphaseOffset = np.pi/4
-
-FLamplitude = np.pi/2
-FLfrequency = 1000
-FLphaseOffset = np.pi
-
-physicsClient = p.connect(p.GUI)
-p.setAdditionalSearchPath(pybullet_data.getDataPath())
-p.setGravity(0,0,-9.8)
-planeId = p.loadURDF("plane.urdf")
-RobotId = p.loadURDF("body.urdf")
-p.loadSDF("world.sdf")
-pyrosim.Prepare_To_Simulate(RobotId)
-backLegSensorValues = np.zeros(1000)
-frontLegSensorValues = np.zeros(1000)
-#targetAngles = np.sin(np.linspace(0, 2*np.pi, 1000))*(np.pi/4)
-BLtargetAngles = np.linspace(0, 2*np.pi, 1000)
-BLtargetAngles = BLamplitude*np.sin(BLfrequency*BLtargetAngles+BLphaseOffset)
-FLtargetAngles = np.linspace(0, 2*np.pi, 1000)
-FLtargetAngles = FLamplitude*np.sin(FLfrequency*FLtargetAngles+FLphaseOffset)
-#np.save(file = "data/sinus.npy", arr = BLtargetAngles)
-#np.save(file = "data/sinusfront.npy", arr = FLtargetAngles)
-#exit()
-for i in range(1000):
-    #print(i)
+simulation = SIMULATION()
+# physicsClient = p.connect(p.GUI)
+# p.setAdditionalSearchPath(pybullet_data.getDataPath())
+# p.setGravity(0,0,c.gravity)
+# planeId = p.loadURDF("plane.urdf")
+# RobotId = p.loadURDF("body.urdf")
+# p.loadSDF("world.sdf")
+# pyrosim.Prepare_To_Simulate(RobotId)
+# backLegSensorValues = np.zeros(c.steps)
+# frontLegSensorValues = np.zeros(c.steps)
+# #targetAngles = np.sin(np.linspace(0, 2*np.pi, 1000))*(np.pi/4)
+# BLtargetAngles = np.linspace(0, c.target_angles, c.steps)
+# BLtargetAngles = c.BLamplitude*np.sin(c.BLfrequency*BLtargetAngles+c.BLphaseOffset)
+# FLtargetAngles = np.linspace(0, c.target_angles, c.steps)
+# FLtargetAngles = c.FLamplitude*np.sin(c.FLfrequency*FLtargetAngles+c.FLphaseOffset)
+# #np.save(file = "data/sinus.npy", arr = BLtargetAngles)
+# #np.save(file = "data/sinusfront.npy", arr = FLtargetAngles)
+# #exit()
+# for i in range(c.steps):
+#     #print(i)
     
-    time.sleep(1/60)
-    p.stepSimulation()
-    backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("Backleg")    
-    frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("Frontleg")
-    #print(backLegSensorValues)
+#     time.sleep(c.time_sleep)
+#     p.stepSimulation()
+#     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("Backleg")    
+#     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("Frontleg")
+#     #print(backLegSensorValues)
     
-    pyrosim.Set_Motor_For_Joint(
-        bodyIndex = RobotId,
-        jointName = b'Torso_Backleg', 
-        controlMode = p.POSITION_CONTROL,
-        targetPosition = BLtargetAngles[i],
-        maxForce = 40)
-    pyrosim.Set_Motor_For_Joint(
-        bodyIndex = RobotId,
-        jointName = b'Torso_Frontleg', 
-        controlMode = p.POSITION_CONTROL,
-        targetPosition = FLtargetAngles[i],
-        maxForce = 40)
-    np.save(file = "data/sensordata.npy", arr = backLegSensorValues)
-    np.save(file = "data/sensordatafront.npy", arr = frontLegSensorValues)
-p.disconnect()
+#     pyrosim.Set_Motor_For_Joint(
+#         bodyIndex = RobotId,
+#         jointName = b'Torso_Backleg', 
+#         controlMode = p.POSITION_CONTROL,
+#         targetPosition = BLtargetAngles[i],
+#         maxForce = c.BLmaxForce)
+#     pyrosim.Set_Motor_For_Joint(
+#         bodyIndex = RobotId,
+#         jointName = b'Torso_Frontleg', 
+#         controlMode = p.POSITION_CONTROL,
+#         targetPosition = FLtargetAngles[i],
+#         maxForce = c.FLmaxForce)
+#     np.save(file = "data/sensordata.npy", arr = backLegSensorValues)
+#     np.save(file = "data/sensordatafront.npy", arr = frontLegSensorValues)
+# p.disconnect()
